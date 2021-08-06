028dd37184f6b3f4259e859f7b98f959bb35c5a4
elamannaMeli <elias.lamanna@mercadolibre.com>
2021-08-05 23:16:38 -0300

    singleton

---
+++ src/main/java/singleton/SingletonClass.java
@@ -0,0 +1,19 @@
+package singleton;
+
+public class SingletonClass {
+    private static SingletonClass instance;
+
+    private SingletonClass(){
+    }
+
+    public static SingletonClass getInstance() {
+        if(instance == null) {
+            instance = new SingletonClass();
+        }
+        return instance;
+    }
+
+    public String saludar() {
+        return "Hola";
+    }
+}
